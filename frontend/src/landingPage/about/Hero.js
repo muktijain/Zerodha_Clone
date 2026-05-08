da2eed8e218b0d581fa728132de24cde8da0810b
Mukti Jain <05muktijain@gmail.com>
2026-05-08 21:45:01 +0530

    hero

--- frontend/src/landingPage/about/Hero.js
+++ frontend/src/landingPage/about/Hero.js
@@ -2,16 +2,19 @@ import React from "react";
 
 function Hero() {
   return (
-    <div className="container mt-5 p-5 mb-5">
-      <div className="row">
-        <h3>
-          We pioneered the discount broking model in India.
+    <div className="container">
+      <div className="row p-5 mt-5 mb-5">
+        <h1 className="fs-2 text-center">
+          We pioneered the discount broking model in India
           <br />
           Now, we are breaking ground with our technology.
-        </h3>
+        </h1>
       </div>
-      <hr />
-      <div className="row">
+
+      <div
+        className="row p-5 mt-5 border-top text-muted"
+        style={{ lineHeight: "1.8", fontSize: "1.2em" }}
+      >
         <div className="col-6 p-5">
           <p>
             We kick-started operations on the 15th of August, 2010 with the goal
@@ -52,4 +55,5 @@ function Hero() {
     </div>
   );
 }
+
 export default Hero;
